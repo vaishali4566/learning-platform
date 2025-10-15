d9f64a8d075850246df7729ef69f813cfd0839cf
vaishali4566 <vaishaliverma@globussoft.in>
2025-10-15 18:20:11 +0530

    added user quiz controller and view

--- app/Http/Controllers/Web/TrainerController.php
+++ app/Http/Controllers/Web/TrainerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Trainer;
