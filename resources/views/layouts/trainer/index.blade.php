4bc6f591e80f8820a0ac922e350f0eda4b6c6b39
vishalglobussoftSDE <vishalvishwakarma@globussoft.in>
2025-12-01 11:33:24 +0530

    add logo to layout

--- resources/views/layouts/trainer/index.blade.php
+++ resources/views/layouts/trainer/index.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('Globbusoft_learning channel logo.jpg') }}">
 
     <title>@yield('title', 'Trainer Dashboard | E-Learning')</title>
 
